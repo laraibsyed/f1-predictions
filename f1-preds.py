6a631cd9465cd8d734492079e338fa11712f703d
Laraib Syed <laraibsyed92@gmail.com>
2026-01-10 20:12:40 +0400

    iteration 1 with 42% accuracy

--- f1-preds.py
+++ f1-preds.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import fastf1 as ff1
 import os
+import seaborn as sns
 from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
-from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import xgboost as xgb
 
 df = pd.read_csv("data/f1_training_data_complete.csv")
 
@@ -72,4 +74,72 @@ columns_to_drop = [
 
 final_df = final_df.drop(columns=columns_to_drop, errors="ignore")
 
+final_df = final_df.sort_values(['Season', 'Round Number'])
+
+numeric_df = final_df.drop(columns=["DriverId", "TeamId", "TeamId_Cleaned"], errors="ignore").select_dtypes(include=['number'])
+corr_matrix = numeric_df.corr()
+
+plt.figure(figsize=(12, 10)) 
+sns.heatmap(
+    corr_matrix, 
+    annot=True, 
+    cmap='coolwarm', 
+    fmt='.2f', 
+    linewidths=.5
+)
+plt.title('Feature Correlation Matrix')
+plt.show()
+
 print("Final List of Columns: ", final_df.columns)
+
+train_df = final_df[final_df["Season"] <= 2024]
+test_df = final_df[final_df["Season"] == 2025]
+
+train_df = train_df.sort_values(["Season", "Round Number"])
+test_df = test_df.sort_values(["Season", "Round Number"])
+
+train_query = train_df.groupby(["Season", "Round Number"]).size().to_list()
+test_query = test_df.groupby(["Season", "Round Number"]).size().to_list()
+
+drop_cols = ["Season", "Round Number", "Position", "DriverId"]
+x_train = train_df.drop(columns=drop_cols)
+y_train = 21 - train_df["Position"]
+
+X_test = test_df.drop(columns=drop_cols)
+y_test = 21 - test_df["Position"]
+
+model = xgb.XGBRanker(objective='rank:pairwise', learning_rate=0.1, n_estimators=100, max_depth=6)
+model.fit(x_train, y_train, group=train_query)
+
+preds = model.predict(X_test)
+
+test_df["Predicted Score"] = preds 
+
+test_df["Predicted Position"] = (
+    test_df.groupby(["Season", "Round Number"])["Predicted Score"]
+    .rank(ascending=False, method="first")
+    .astype(int)
+)
+
+matches = test_df[
+    (test_df["Predicted Position"] == 1) & 
+    (test_df["Position"] == 1)
+]
+
+accuracy = len(matches) / len(test_query)
+print(f"🏆 Winner Prediction Accuracy: {accuracy:.2%}")
+
+print("\n--- Example Race Prediction ---")
+example_race = test_df[test_df["Round Number"] == 1][["DriverId", "Predicted Position", "Position"]]
+print(example_race.sort_values("Predicted Position").head(5))
+
+importance = model.feature_importances_
+feature_names = x_train.columns
+
+plt.figure(figsize=(10, 6))
+plt.barh(feature_names, importance)
+plt.title("What Matters to the Model?")
+plt.xlabel("Importance Score")
+plt.ylabel("Features")
+plt.gca().invert_yaxis()
+plt.show()
